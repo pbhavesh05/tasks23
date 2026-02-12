bb2e95b169bec18dbe8c58ca45646daaea18e4d1
pbhavesh05 <pbhavesh@udel.edu>
2026-02-12 08:18:46 -0500

    Finished the HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,51 @@
 import React from "react";
 import "./App.css";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
-function App(): React.JSX.Element {
-    return (
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>Bhavesh Pothineni</p> 
-            <p>Hello World</p>
+function App(): JSX.Element { 
+    return ( 
+        <div className = "App"> 
+
+
+            <header className = "App-Header">
+                <h1>UD CISC275 - This is Header, My name is Bhavesh</h1>
+            </header> 
+            <img src ="https://upload.wikimedia.org/wikipedia/commons/9/94/LeBron_James_%2815823062226%29.jpg" 
+            alt = "Lebron James in Cleveland"/> 
+            
+            Ordered List: 
+            <ol> 
+                <li>Green</li>
+                <li>Blue</li>
+                <li>Red</li>
+            </ol>
+            
+            <Button onClick={ () => { console.log("Hello World!") } }>Log Hello World!</Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red"
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red"
+                            }}
+                        />
+                    </Col>
+                </Row>
+            </Container>
+              
         </div>
-    );
-}
 
-export default App;
+    )
+}
+export default App
