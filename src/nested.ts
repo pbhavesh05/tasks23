287b8631509c1c5b3e54bf5f4116bb33337b4264
pbhavesh05 <pbhavesh@udel.edu>
2026-03-05 10:52:53 -0500

    Complete

--- src/nested.ts
+++ src/nested.ts
@@ -1,4 +1,3 @@
-import { queryAllByRole } from "@testing-library/dom";
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
 import { duplicateQuestion, makeBlankQuestion } from "./objects";
