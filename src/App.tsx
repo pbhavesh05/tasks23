85bc0a04deaf0b92924c6d0779bfbc125b8ecd91
pbhavesh05 <pbhavesh@udel.edu>
2026-03-12 21:59:42 -0400

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { type JSX } from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
