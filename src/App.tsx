79346e616947d5790563966a90d5c90e29a771f9
pbhavesh05 <pbhavesh@udel.edu>
2026-03-12 22:06:09 -0400

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { type JSX } from "react";
+import React from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
@@ -7,7 +7,7 @@ import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 
-function App(): JSX.Element {
+function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
