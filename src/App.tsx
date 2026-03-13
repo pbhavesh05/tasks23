7922d353bfeb6ee11c5451a4963186aabce6022e
pbhavesh05 <pbhavesh@udel.edu>
2026-03-12 21:51:20 -0400

    Copy working code from solved-nested into solved-state

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,31 @@
 import React from "react";
 import "./App.css";
-import { Button, Container, Row, Col } from "react-bootstrap";
+import { ChangeType } from "./components/ChangeType";
+import { RevealAnswer } from "./components/RevealAnswer";
+import { StartAttempt } from "./components/StartAttempt";
+import { TwoDice } from "./components/TwoDice";
+import { CycleHoliday } from "./components/CycleHoliday";
+import { Counter } from "./components/Counter";
 
-function App() { 
-    return ( 
-        <div className = "App"> 
-
-
-            <header className = "App-Header">
-                <h1>UD CISC275 - This is Header, My name is Bhavesh</h1>
-            </header> 
-            <img src ="https://upload.wikimedia.org/wikipedia/commons/9/94/LeBron_James_%2815823062226%29.jpg" 
-            alt = "Lebron James in Cleveland"/> 
-            
-            Ordered List: 
-            <ol> 
-                <li>Green</li>
-                <li>Blue</li>
-                <li>Red</li>
-            </ol>
-            
-            <Button onClick={ () => { console.log("Hello World!") } }>Log Hello World!</Button>
-            <Container>
-                <Row>
-                    <Col>
-                        <div
-                            style={{
-                                width: "100px",
-                                height: "100px",
-                                backgroundColor: "red"
-                            }}
-                        />
-                    </Col>
-                    <Col>
-                        <div
-                            style={{
-                                width: "100px",
-                                height: "100px",
-                                backgroundColor: "red"
-                            }}
-                        />
-                    </Col>
-                </Row>
-            </Container>
-              
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">
+                UD CISC275 with React Hooks and TypeScript
+            </header>
+            <hr></hr>
+            <Counter></Counter>
+            <hr />
+            <RevealAnswer></RevealAnswer>
+            <hr />
+            <StartAttempt></StartAttempt>
+            <hr />
+            <TwoDice></TwoDice>
+            <hr />
+            <ChangeType></ChangeType>
+            <hr />
+            <CycleHoliday></CycleHoliday>
         </div>
-
-    )
+    );
 }
-export default App
+export default App;
--- src/objects.ts
+++ src/objects.ts
@@ -10,18 +10,16 @@ export function makeBlankQuestion(
     name: string,
     type: QuestionType
 ): Question {
-    return{ 
-        id:id, 
-        name:name, 
-        type: type, 
-        body: "", 
-        expected: "", 
-        options: [], 
-        points: 1, 
-        published: false  
-    }
-
-
+    return {
+        id,
+        name,
+        type,
+        body: "",
+        options: [],
+        expected: "",
+        points: 0,
+        published: false
+    };
 }
 
 /**
@@ -31,12 +29,8 @@ export function makeBlankQuestion(
  *
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
-export function isCorrect(question: Question, answer: string): boolean { 
-    const trimmedAnswer= answer.toLowerCase().trim();  
-    const trimExpected = question.expected.toLowerCase().trim(); 
-
-
-    return trimmedAnswer === trimExpected;
+export function isCorrect(question: Question, answer: string): boolean {
+    return false;
 }
 
 /**
@@ -46,11 +40,7 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-
-    if(question.type === "multiple_choice_question"){ 
-        return question.options.includes(answer); 
-    }
-    return true; 
+    return false;
 }
 
 /**
@@ -59,9 +49,8 @@ export function isValid(question: Question, answer: string): boolean {
  * separated by ": ". So for example, the question with id 9 and the
  * name "My First Question" would become "9: My First Q".
  */
-export function toShortForm(question: Question): string { 
-    const shortened = question.name.slice(0,10); 
-    return `${question.id}: ${shortened}` ;
+export function toShortForm(question: Question): string {
+    return "";
 }
 
 /**
@@ -82,19 +71,7 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    const header =  `# ${question.name}`;  
-    const body = question.body; 
-
-    if (question.type === `multiple_choice_question`){ 
-        const o = question.options.map((option: string):string=> {return `- ${option}`}) 
-        return `${header}\n${body}\n${o.join(`\n`)}`
-
-        
-    }
-    return `${header}\n${body}`
-
-
-    
+    return "";
 }
 
 /**
@@ -102,7 +79,7 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return {...question,name : newName }; 
+    return question;
 }
 
 /**
@@ -110,8 +87,8 @@ export function renameQuestion(question: Question, newName: string): Question {
  * should be inverted. If the question was not published, now it should be
  * published; if it was published, now it should be not published.
  */
-export function publishQuestion(question: Question): Question { 
-    return {...question,published:!question.published}
+export function publishQuestion(question: Question): Question {
+    return question;
 }
 
 /**
@@ -121,7 +98,7 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return { ...oldQuestion,id:id, name: `Copy of ${oldQuestion.name}`, published:false}; 
+    return oldQuestion;
 }
 
 /**
@@ -132,7 +109,7 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return {...question,options: [...question.options, newOption]};
+    return question;
 }
 
 /**
@@ -149,5 +126,5 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number }
 ): Question {
-    return {...contentQuestion, id:id, name:name, points:points, published:false};
+    return contentQuestion;
 }
